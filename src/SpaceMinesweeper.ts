36f2df0dd929ea3b1223b9cf7ec464fbc684338c
Evan Suma Rosenberg <suma@umn.edu>
2023-01-26 16:49:54 -0600

    class update

--- src/SpaceMinesweeper.ts
+++ src/SpaceMinesweeper.ts
@@ -10,6 +10,10 @@ export class SpaceMinesweeper extends gfx.GfxApp
 {
     // The graphics primitives that define objects in the scene
     private ship: gfx.Rectangle;
+    private star: gfx.Rectangle;
+
+    // The stars will be drawn using a 2D particle system
+    private starfield: gfx.Particles2;
 
     private mousePosition: gfx.Vector2;
 
@@ -20,8 +24,13 @@ export class SpaceMinesweeper extends gfx.GfxApp
         super();
 
         this.ship = new gfx.Rectangle();
+        this.star = new gfx.Rectangle();
+
+        this.starfield = new gfx.Particles2(this.star, 200);
 
         this.mousePosition = new gfx.Vector2();
+
+        this.renderer.viewport = gfx.Viewport.CROP;
     }
 
     createScene(): void 
@@ -31,7 +40,18 @@ export class SpaceMinesweeper extends gfx.GfxApp
         this.ship.material.texture = new gfx.Texture('./ship.png');
         this.ship.scale.set(0.08, 0.08);
 
+        this.star.material.texture = new gfx.Texture('./star.png');
+
+        for(let i=0; i < this.starfield.numParticles; i++)
+        {
+            this.starfield.particleSizes[i] = Math.random()*0.008 + 0.002;
+            this.starfield.particlePositions[i].set(Math.random()*2-1, Math.random()*2-1);
+        }
+
+        this.starfield.update(true, true);
+
         this.scene.add(this.ship);
+        this.scene.add(this.starfield);
     }
 
     update(deltaTime: number): void 
