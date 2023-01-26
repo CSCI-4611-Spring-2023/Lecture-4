e9b96d0ed13ad3142bd02a06b25650c7a88f2ced
Evan Suma Rosenberg <suma@umn.edu>
2023-01-26 16:22:05 -0600

    class update

--- src/SpaceMinesweeper.ts
+++ src/SpaceMinesweeper.ts
@@ -8,16 +8,26 @@ import * as gfx from 'gophergfx'
 
 export class SpaceMinesweeper extends gfx.GfxApp
 {
+    // The graphics primitives that define objects in the scene
+    private ship: gfx.Rectangle;
+
     constructor()
     {
         // The first line of any child class constructor must call
         // the base class's constructor using the super() method. 
         super();
+
+        this.ship = new gfx.Rectangle();
     }
 
     createScene(): void 
     {
-        
+        // Load the ship texture to make the object a sprite, then scale it 
+        // to an appropriate size.
+        this.ship.material.texture = new gfx.Texture('./ship.png');
+        this.ship.scale.set(0.08, 0.08);
+
+        this.scene.add(this.ship);
     }
 
     update(deltaTime: number): void 
